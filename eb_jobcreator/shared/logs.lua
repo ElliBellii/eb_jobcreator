c50b1aaa197c3a7b1a4f7188bc64177fc101a183
ElliBellii <166251927+ElliBellii@users.noreply.github.com>
2024-05-23 17:04:38 +0200

    Update logs.lua

--- eb_jobcreator/shared/logs.lua
+++ eb_jobcreator/shared/logs.lua
@@ -1,4 +1,4 @@
 Logs = {}
 
 Logs.EnableLogs = true
-Logs.Webhook = 'https://discord.com/api/webhooks/1243181196213485698/aia8vwLF7JDpxWqh-dHjA1g3e9GIGNuJruJPvtBAtbrqAR64PC4SL7-VwfoucGlzurMf'
+Logs.Webhook = 'WEBHOOK_HERE'
